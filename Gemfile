2a1eee4a989c8e056842b47eeab8aca20f87f889
Marvin Pinto <me@marvinp.ca>
2014-11-12 11:30:18 -0500

    Refactor the sabnzbd job to stop using rest_client

--- Gemfile
+++ Gemfile
@@ -2,5 +2,4 @@ source 'https://rubygems.org'
 
 gem 'dashing'
 gem 'dotenv'
-gem 'rest_client'
 gem 'logger'
--- jobs/sabnzbd.rb
+++ jobs/sabnzbd.rb
@@ -1,19 +1,32 @@
-require 'rest_client'
+require 'net/http'
 require 'logger'
 
 SCHEDULER.every '1s', :first_in => 0 do |job|
 
   logger = Logger.new(STDOUT)
-  logger.level = Logger::DEBUG unless ENV['RACK_ENV'] == 'production'
+  logger.level = Logger::INFO
   server_status = true
 
   begin
-    http_response = RestClient.get ENV['SABNZBD_API_URL'], {:params =>{'mode' => 'queue', 'start' => 'START', 'limit' => 'LIMIT', 'output' => 'json'}}
-  rescue Exception => e
-    server_status = false
+    uri = URI.parse(ENV['SABNZBD_API_URL'])
+    params = {'mode' => 'queue',
+              'start' => 'START',
+              'limit' => 'LIMIT',
+              'output' => 'json'}
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.path)
+    request.set_form_data(params)
+    response = http.request(request)
+  rescue StandardError => e
     logger.error 'Could not connect to %s' % ENV['SABNZBD_API_URL']
+    logger.debug e
+    server_status = false
   end
 
-  send_event('media_overview', { sabnzbd_status: (server_status ? http_response.code == 200 : false) })
+  if server_status and response_code = '200'
+    send_event('media_overview', { sabnzbd_status: true })
+  else
+    send_event('media_overview', { sabnzbd_status: false })
+  end
 
 end
