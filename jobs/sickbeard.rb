38b31650ffea598e45fdebe1bf2a479591a165d3
Marvin Pinto <me@marvinp.ca>
2014-11-12 21:57:25 -0500

    Refactor sickbeard to use params + correct the web url

--- jobs/sickbeard.rb
+++ jobs/sickbeard.rb
@@ -7,15 +7,14 @@ SCHEDULER.every '1s', :first_in => 0 do |job|
   logger.level = Logger::INFO
   server_status = true
 
-  url_string = ENV['SICKBEARD_API_URL'] +
-    '/' +
-    ENV['SICKBEARD_API_KEY'] +
-    '/?cmd=sb'
+  url_string = ENV['SICKBEARD_API_URL'] + '/' + ENV['SICKBEARD_API_KEY']
 
   begin
     uri = URI.parse(url_string)
+    params = {'cmd' => 'sb'}
+    uri.query = URI.encode_www_form(params)
     http = Net::HTTP.new(uri.host, uri.port)
-    request = Net::HTTP::Get.new(uri.path)
+    request = Net::HTTP::Get.new(uri.to_s)
     response = http.request(request)
   rescue StandardError => e
     logger.error 'Could not connect to %s' % ENV['SICKBEARD_API_URL']
