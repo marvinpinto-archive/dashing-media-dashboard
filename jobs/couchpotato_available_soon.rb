1b4e533b740ac54a6ed523399f7e058c68c27a52
Marvin Pinto <me@marvinp.ca>
2014-12-08 13:34:39 -0500

    Use the posters widget to display Upcoming couchpotato movies

---
+++ jobs/couchpotato_available_soon.rb
@@ -0,0 +1,45 @@
+require 'net/http'
+require 'logger'
+require 'json'
+SCHEDULER.every '5m', :first_in => 0 do |job|
+
+  logger = Logger.new(STDOUT)
+  logger.level = Logger::INFO
+  server_status = true
+
+  url_string = ENV['COUCHPOTATO_API_URL'] +
+    '/' +
+    ENV['COUCHPOTATO_API_KEY'] +
+    '/dashboard.soon/'
+
+  begin
+    uri = URI.parse(url_string)
+    params = {'type' => 'movie',
+              'limit_offset' => '12,0',
+              'random' => 'true'}
+    uri.query = URI.encode_www_form(params)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Get.new(uri.request_uri)
+    response = http.request(request)
+  rescue StandardError => e
+    logger.error 'Could not connect to %s' % ENV['COUCHPOTATO_API_URL']
+    logger.debug e
+    server_status = false
+  end
+
+  if server_status and response.code == '200'
+    result = JSON.parse(response.body)
+    movie_array = Array.new
+    result['movies'].each do |child|
+      movie_data = {'link' => 'http://www.imdb.com/title/' + child['info']['imdb'],
+                    'name' => child['info']['original_title'],
+                    'jpgsrc' => child['info']['images']['poster_original'][0]}
+      movie_array.push(movie_data)
+    end
+    send_event('available_soon_movies', { posters_raw: movie_array })
+  else
+    logger.error '%s is not reachable the moment' % ENV['COUCHPOTATO_API_URL']
+    send_event('available_soon_movies', { posters_raw: [] })
+  end
+
+end
